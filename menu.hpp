b4308eab87d73c7f0d557427a05942db5b2964fc
cr7fanboycool <essa30808@gmail.com>
2025-04-29 17:37:17 +0400

    Add files via upload

--- menu.hpp
+++ menu.hpp
@@ -1,7 +1,23 @@
-#pragma once
-#include <imgui.h>
+bool menu_open = false; // toggle flag, if not already global
 
-if (menu_open == true) {
-    // draw menu
+void RenderMenu() {
+    static bool menu_open = false;
+
+    // Toggle with Insert
+    if (GetAsyncKeyState(VK_INSERT) & 1)
+        menu_open = !menu_open;
+
+    if (!menu_open)
+        return;
+
+    ImGui_ImplDX11_NewFrame();
+    ImGui_ImplWin32_NewFrame();
+    ImGui::NewFrame();
+
+    ImGui::Begin("HVH Menu", &menu_open, ImGuiWindowFlags_AlwaysAutoResize);
+    render_hvh_tab(); // your HVH tab from menu.cpp
+    ImGui::End();
+
+    ImGui::Render();
+    ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
 }
-    
