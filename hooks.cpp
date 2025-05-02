618b26549ede4cc0108af7ddfc419a6e30def246
cr7fanboycool <essa30808@gmail.com>
2025-05-02 15:00:45 +0400

    Add files via upload

--- hooks.cpp
+++ hooks.cpp
@@ -1,25 +1,49 @@
-// In your Present hook:
-void __stdcall hkPresent(IDXGISwapChain* swap_chain, UINT sync_interval, UINT flags) {
-    static bool init = false;
-    if (!init) {
-        ID3D11Device* device;
-        ID3D11DeviceContext* context;
-        swap_chain->GetDevice(__uuidof(ID3D11Device), (void**)&device);
-        device->GetImmediateContext(&context);
-        
-        menu::initialize(device, context);
-        init = true;
+#include <d3d11.h>
+#include <dxgi.h>
+#include <Windows.h>
+#include <iostream>
+#include "menu.hpp"
+#include "imgui/imgui.h"
+#include "imgui/backends/imgui_impl_win32.h"
+#include "imgui/backends/imgui_impl_dx11.h"
+
+typedef HRESULT(__stdcall* PresentFn)(IDXGISwapChain*, UINT, UINT);
+PresentFn oPresent = nullptr;
+
+ID3D11Device* g_pd3dDevice = nullptr;
+ID3D11DeviceContext* g_pd3dDeviceContext = nullptr;
+ID3D11RenderTargetView* g_mainRenderTargetView = nullptr;
+HWND g_hWnd = nullptr;
+
+bool initialized = false;
+
+HRESULT __stdcall hooked_present(IDXGISwapChain* pSwapChain, UINT SyncInterval, UINT Flags) {
+    if (!initialized) {
+        if (SUCCEEDED(pSwapChain->GetDevice(__uuidof(ID3D11Device), (void**)&g_pd3dDevice))) {
+            g_pd3dDevice->GetImmediateContext(&g_pd3dDeviceContext);
+
+            DXGI_SWAP_CHAIN_DESC sd;
+            pSwapChain->GetDesc(&sd);
+            g_hWnd = sd.OutputWindow;
+
+            ID3D11Texture2D* pBackBuffer = nullptr;
+            pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer);
+            g_pd3dDevice->CreateRenderTargetView(pBackBuffer, nullptr, &g_mainRenderTargetView);
+            pBackBuffer->Release();
+
+            ImGui::CreateContext();
+            ImGui::StyleColorsDark();
+            ImGui_ImplWin32_Init(g_hWnd);
+            ImGui_ImplDX11_Init(g_pd3dDevice, g_pd3dDeviceContext);
+
+            initialized = true;
+        }
     }
 
-    // Handle input and render
-    menu::handle_input();
-    
-    ImGui_ImplDX11_NewFrame();
-    ImGui_ImplWin32_NewFrame();
-    ImGui::NewFrame();
-    
-    menu::render();
-    
-    ImGui::Render();
-    ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
+    if (initialized) {
+        g_pd3dDeviceContext->OMSetRenderTargets(1, &g_mainRenderTargetView, nullptr);
+        RenderMenu();  // your menu from menu.cpp
+    }
+
+    return oPresent(pSwapChain, SyncInterval, Flags);
 }
